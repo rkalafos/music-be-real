21cf86ced41f4ca454b885aecbf6d5f361239090
Rachel Kalafos <kalafos.r@northeastern.edu>
2022-04-17 13:16:16 -0400

    More basic routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from "./components/home";
 import Profile from "./components/profile";
+import Details from "./components/details";
+import Search from "./components/search";
+import Login from "./components/login";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
           <Routes>
               <Route index element={<Home/>} />
               <Route path="/profile/:profileId?" element={<Profile/>} />
+              <Route path="/details/:did" element={<Details />}/>
+              <Route path="/search" element={<Search />} />
+              <Route path="/login" element={<Login />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/components/details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Details = () => {
+    return(
+        <>
+            <h1>Details Page</h1>
+        </>
+    )
+};
+export default Details;
