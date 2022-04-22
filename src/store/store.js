7d8020bdf348903e62e310df3180bcbd1a306c04
anikarabenhorst <anikacchase@gmail.com>
2022-04-21 23:09:08 -0400

    first pass

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import postReducer from '../reducers/songChoiceReducer.js';
+import postReducer from '../reducers/postReducer.js';
 import songChoiceReducer from "../reducers/songChoiceReducer.js";
 import tokenReducer from "../reducers/tokenReducer";
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
