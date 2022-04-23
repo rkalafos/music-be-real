c411b7ffba4e24329f29247fe6660066aa7b3786
Rachel Kalafos <kalafos.r@northeastern.edu>
2022-04-22 22:58:33 -0400

    basic difference home page

---
+++ src/components/PostList/index.js
@@ -0,0 +1,9 @@
+
+
+
+const PostList = () => {
+    return(
+        <div>This is the list of posts.</div>
+    )
+};
+export default PostList;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,8 @@
-import React from 'react';
-// import {useSelector} from "react-redux";
+import React, {useState} from 'react';
 import {DefaultLayout} from "../layouts/DefaultLayout";
-import {Box, Button, Flex} from "@chakra-ui/react";
+import {Box, Button, Flex, GridItem, Spacer, Grid} from "@chakra-ui/react";
 import {useNavigate} from "react-router";
+import PostList from "../components/PostList";
 
 
 const HomePage = () => {
@@ -16,25 +16,57 @@ const HomePage = () => {
     const token = useSelector(state => state.token.token);
      */
     const navigate = useNavigate();
+    const [userId, setUserId] = useState(undefined);
     return(
         <DefaultLayout>
-            <Flex align={"center"}>
-                <Box align={"right"}>
-                    <Button
-                        mr={4}
-                        colorScheme={"red"}
-                        onClick={() => navigate("/register")}
-                    >
-                        Register
-                    </Button>
-                    <Button
-                        colorScheme={"teal"}
-                        onClick={() => navigate("/login")}
-                    >
-                        Login
-                    </Button>
-                </Box>
-            </Flex>
+            <Grid
+                w={"100%"}
+                templateRows='repeat(3, 1fr)'
+                templateColumns='repeat(3, 1fr)'
+                gap={4}
+            >
+                <GridItem>
+                    <Button onClick={() => {setUserId(userId ? undefined : "rkalafos")}}>TEMPORARY LOGIN TOGGLE</Button>
+                </GridItem>
+                <GridItem>
+
+                </GridItem>
+                <GridItem>
+                    <Flex w={"100%"}>
+                        <Spacer />
+                        <Box>
+                            {userId ? <Button onClick={() => navigate(`/profile/${userId}`)} mr={4}>View Profile</Button> :
+                                <div>
+                                    <Button
+                                        mr={4}
+                                        colorScheme={"red"}
+                                        onClick={() => navigate("/register")}
+                                    >
+                                        Register
+                                    </Button>
+                                    <Button
+                                        mr={4}
+                                        colorScheme={"teal"}
+                                        onClick={() => navigate("/login")}
+                                    >
+                                        Login
+                                    </Button>
+                                </div>
+                            }
+                        </Box>
+                    </Flex>
+                </GridItem>
+                <GridItem  />
+                <GridItem bg="white">
+                    {userId ? <PostList /> : (
+                        <div>
+                            Join MusicBeReal to enjoy all the perks!
+                        </div>
+                    )}
+                </GridItem>
+                <GridItem  />
+            </Grid>
+
             { /*
             !token ?
                 <a href={`${REACT_APP_AUTHORIZE_URL}?client_id=${REACT_APP_CLIENT_ID}&redirect_uri=${REACT_APP_REDIRECT_URL}&response_type=${RESPONSE_TYPE}`}>
