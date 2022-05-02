3fcd8358a9fd028b3f531355e8749fa98141c51f
Dani <dani10.rowe@gmail.com>
2022-05-01 21:17:37 -0400

    teeny bug fix, correct initials

--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -64,7 +64,7 @@ const ProfilePage = () => {
               <Center>
                 <Avatar
                     size="md"
-                    name={currentUser?.username}
+                    name={profileUser?.username}
                     src={profileUser?.avatarImage}
                 ></Avatar>
                 <Heading>
